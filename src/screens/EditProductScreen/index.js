c32e0ad83dd055c4ee9f78c84de75fa3ec788114
SangCao1995 <caobathanhsang@gmail.com>
2021-07-08 17:42:01 +0700

    Add useReducer for TextInput

--- src/screens/EditProductScreen/index.js
+++ src/screens/EditProductScreen/index.js
@@ -1,5 +1,12 @@
-import React, {useState, useCallback} from 'react';
-import {View, Text, ScrollView, TextInput, StyleSheet} from 'react-native';
+import React, {useCallback, useReducer} from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  Alert,
+} from 'react-native';
 import {Header} from '../../components';
 import {Colors} from '../../themes';
 import {useDispatch} from 'react-redux';
@@ -7,33 +14,88 @@ import {productActions} from '../../store/actions';
 
 export const EditProductScreen = props => {
   const product = props.route.params.product;
-  const [title, setTitle] = useState(product ? product.title : '');
-  const [imageUrl, setImageUrl] = useState(product ? product.imageUrl : '');
-  const [price, setPrice] = useState('');
   const dispatch = useDispatch();
-  const [description, setDescription] = useState(
-    product ? product.description : '',
-  );
+  const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
+  const formReducer = (state, action) => {
+    if (action.type === FORM_INPUT_UPDATE) {
+      const updatedValues = {
+        ...state.inputValues,
+        [action.input]: action.value,
+      };
+      const updatedInputValidities = {
+        ...state.inputValidities,
+        [action.input]: action.isValid,
+      };
+      let updatedFormIsValid = true;
+      for (const key in updatedInputValidities) {
+        updatedFormIsValid = updatedFormIsValid && updatedInputValidities[key];
+      }
+      return {
+        ...state,
+        inputValues: updatedValues,
+        inputValidities: updatedInputValidities,
+        formIsValid: updatedFormIsValid,
+      };
+    }
+    return state;
+  };
+  const [formState, dispatchFormState] = useReducer(formReducer, {
+    inputValues: {
+      title: product ? product.title : '',
+      imageUrl: product ? product.imageUrl : '',
+      price: '',
+      description: product ? product.description : '',
+    },
+    inputValidities: {
+      title: product ? true : false,
+      imageUrl: product ? true : false,
+      price: product ? true : false,
+      description: product ? true : false,
+    },
+    formIsValid: product ? true : false,
+  });
+
   const submitHandle = useCallback(() => {
+    if (!formState.formIsValid) {
+      Alert.alert('Wrong input!', 'Please check the errors in the form.', [
+        {
+          text: 'OK',
+        },
+      ]);
+      return;
+    }
     if (product) {
       const data = {
         id: product.id,
-        title,
-        imageUrl,
-        description,
+        title: formState.inputValues.title,
+        imageUrl: formState.inputValues.imageUrl,
+        description: formState.inputValues.description,
       };
       dispatch(productActions.editProduct(data));
     } else {
       const data = {
-        title,
-        imageUrl,
-        price: Number(price),
-        description,
+        title: formState.inputValues.title,
+        imageUrl: formState.inputValues.imageUrl,
+        price: Number(formState.inputValues.price),
+        description: formState.inputValues.description,
       };
       dispatch(productActions.createProduct(data));
     }
     props.navigation.goBack();
-  }, [dispatch, title, imageUrl, price, description, product.id]);
+  }, [dispatch, formState, product.id]);
+
+  const titleChangeHandle = (inputIdentifier, text) => {
+    let isValid = false;
+    if (text.trim().length > 0) {
+      isValid = true;
+    }
+    dispatchFormState({
+      type: FORM_INPUT_UPDATE,
+      value: text,
+      input: inputIdentifier,
+      isValid: isValid,
+    });
+  };
 
   return (
     <View style={{flex: 1}}>
@@ -49,35 +111,51 @@ export const EditProductScreen = props => {
             <Text style={styles.label}>Title</Text>
             <TextInput
               style={styles.textInput}
-              value={title}
-              onChangeText={text => setTitle(text)}
+              value={formState.inputValues.title}
+              onChangeText={titleChangeHandle.bind(this, 'title')}
+              keyboardType={'default'}
+              autoCapitalize={'sentences'}
+              autoCorrect
             />
+            {!formState.inputValidities.title && (
+              <Text>Please enter a valid title!</Text>
+            )}
           </View>
           <View style={styles.formControl}>
             <Text style={styles.label}>Image URL</Text>
             <TextInput
               style={styles.textInput}
-              value={imageUrl}
-              onChangeText={text => setImageUrl(text)}
+              value={formState.inputValues.imageUrl}
+              onChangeText={titleChangeHandle.bind(this, 'imageUrl')}
             />
+            {!formState.inputValidities.imageUrl && (
+              <Text>Please enter a valid title!</Text>
+            )}
           </View>
           {product ? null : (
             <View style={styles.formControl}>
               <Text style={styles.label}>Price</Text>
               <TextInput
                 style={styles.textInput}
-                value={price}
-                onChangeText={text => setPrice(text)}
+                value={formState.inputValues.price}
+                onChangeText={titleChangeHandle.bind(this, 'price')}
+                keyboardType={'decimal-pad'}
               />
+              {!formState.inputValidities.price && (
+                <Text>Please enter a valid title!</Text>
+              )}
             </View>
           )}
           <View style={styles.formControl}>
             <Text style={styles.label}>Description</Text>
             <TextInput
               style={styles.textInput}
-              value={description}
-              onChangeText={text => setDescription(text)}
+              value={formState.inputValues.description}
+              onChangeText={titleChangeHandle.bind(this, 'description')}
             />
+            {!formState.inputValidities.description && (
+              <Text>Please enter a valid title!</Text>
+            )}
           </View>
         </View>
       </ScrollView>
